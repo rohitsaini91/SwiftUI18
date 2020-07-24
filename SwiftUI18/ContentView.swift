92028d8b485dd850eae132c64163f29967876e70
Rohit Saini <sketchlearn7@gmail.com>
2020-07-24 09:56:36 +0530

    Code Updated

--- SwiftUI18/ContentView.swift
+++ SwiftUI18/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            NavigationLink(destination: page2()) {
+                Text("page")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+struct page2:View {
+    var body: some View{
+        NavigationLink(destination: page3()){
+            Text("page 3")
+        }
+    }
+}
+
+struct page3:View {
+    var body: some View{
+        Text("Page 3")
     }
 }
